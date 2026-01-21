cd711c2d62efd1fd111b845adcfad8529a70d2a9
Antonio Amaro <ja.antonioamaro@gmail.com>
2026-01-21 14:01:19 -0600

    refactor: changing properties Product.js

--- src/js/models/Product.js
+++ src/js/models/Product.js
@@ -3,8 +3,10 @@ export class Product {
     constructor(name, price, category, description) {
         this.id = Date.now()
         this.name = name
+        this.description = description
+        this.temperature = temperature
+        this.additional = additional
         this.price = price
         this.category = category
-        this.description = description
     }
 }
